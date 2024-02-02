f2a8ba9a41c125a67bbafceef93482edde0ca580
Robert Platt <robp2602@gmail.com>
2024-02-02 16:51:36 +0000

    Add basic interactive plot

--- n2n4m/crism_image.py
+++ n2n4m/crism_image.py
@@ -1,11 +1,13 @@
 import os
 import matplotlib.pyplot as plt
-
+import numpy as np
+from ipywidgets import widgets, interactive, IntSlider
 
 from n2n4m import io
 from n2n4m.wavelengths import ALL_WAVELENGTHS, PLEBANI_WAVELENGTHS
 from crism_ml.io import image_shape
 
+BAND_MASK = [True if wavelength in PLEBANI_WAVELENGTHS else False for wavelength in ALL_WAVELENGTHS]
 
 class CRISMImage:
     """
@@ -32,7 +34,6 @@ class CRISMImage:
     -------
 
     """
-
     def __init__(self, filepath: str):
         if not os.path.exists(filepath):
             raise FileNotFoundError(f"File {filepath} not found.")
@@ -73,20 +74,92 @@ class CRISMImage:
         self.n2n4m_denoised = None
         pass
 
-    def single_band_plot(self, ax: plt.Axes = None, band: int = 0) -> None:
-        pass
-
-    def false_colour_plot(self, ax: plt.Axes = None) -> None:
+    def get_false_colours(self) -> None:
+        self.false_colour_im = None
         pass
 
     def plot_spectrum(
         self,
-        pixel: tuple,
-        ax: plt.Axes = None,
-        range: tuple | None = None,
-        bands: tuple = ALL_WAVELENGTHS,
+        pixel: np.ndarray,
+        ax: plt.Axes | None = None,
+        title: str | None = None,
     ) -> None:
-        pass
+        """Plot spectrum of a pixel (1D)"""
+        if len(pixel.shape) != 1:
+            raise ValueError("Pixel must be 1D.")
+        if ax is None:
+            fig, ax = plt.subplots(figsize=(4, 2.5))
+        else:
+            fig = ax.get_figure()
+        
+        if len(pixel) == len(ALL_WAVELENGTHS):
+            pixel = pixel[BAND_MASK]
+        
+        ax.plot(PLEBANI_WAVELENGTHS, pixel)
+        ax.set_yticks([])
+        ax.set_ylabel("Reflectance (I/F)")
+        ax.set_xlabel("Wavelength (μm)")
+        if title is None:
+            ax.set_title(self.im_name)
+        else: ax.set_title(title)
+        return fig, ax
 
-    def plot_summary_parameter(self, parameter: str, ax: plt.Axes = None) -> None:
-        pass
+    def plot_image(
+        self,
+        image: np.ndarray,
+        title: str | None = None,
+        ax: plt.Axes | None = None,
+    ) -> None:
+        """Plot 2D representation of hyperspectral datacube.
+        
+        Parameters
+        ----------
+        image : np.ndarray
+            2D representation of the image.
+        title : str, optional
+            Title of the plot.
+            If None, the title will be the name of the image.
+        ax : plt.Axes, optional
+            Axes object to plot the image on.
+            If None, a new figure and axes will be created.
+        """
+        if len(image.shape) != 2:
+            raise ValueError("Image must be 2D.")
+        
+        if ax is None:
+            fig, ax = plt.subplots()
+        else:
+            fig = ax.get_figure()
+
+        ax.imshow(image)
+        if title is None:
+            ax.set_title(self.im_name)
+        else:
+            ax.set_title(title)
+
+        ax.set_axis_off()
+        return fig, ax
+    
+    def interactive_plot(self) -> None:
+        """Interactive plot of the image.
+        """
+        def update_plots(x, y, band_num):
+
+            fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+            if band_num == "":
+                band_num = 60
+            self.plot_image(self.image[:, :, int(band_num)], ax=ax[0])
+            
+            ax[0].scatter(x, y, marker='x', color='red', label="Selected Pixel")
+
+            self.plot_spectrum(self.image[y, x], ax=ax[1])
+
+            plt.tight_layout()
+            plt.show()
+        x_slider = IntSlider(min=0, max=self.spatial_dims[1]-1, value=0, step=1, description='X:')
+        y_slider = IntSlider(min=0, max=self.spatial_dims[0]-1, value=0, step=1, description='Y:')
+        band_to_display = widgets.Text(value="60", placeholder="0-438", description="Band:", continuous_update=False)
+
+        # Create interactive widget
+        interactive_plot = interactive(update_plots, x=x_slider, y=y_slider, band_num=band_to_display)
+        return interactive_plot
