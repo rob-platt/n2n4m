8e1366a300b350bdde4cc8cefc536eb84db0668e
Robert Platt <rp1818@ic.ac.uk>
2024-02-07 15:23:22 +0000

    Update to v2 load_image, remove mat dict

--- n2n4m/io.py
+++ n2n4m/io.py
@@ -5,38 +5,13 @@ from spectral.io import envi, spyfile
 import crism_ml.io
 
 NUM_BANDS = 438
+    
 
-def crism_to_mat(fname: str, flatten: bool = False) -> dict:
-    """Convert a CRISM ENVI image to a Matlab-like dictionary.
-    Modified from https://github.com/Banus/crism_ml/blob/master/crism_ml/io.py
-    Modification loads all CRISM L bands (0-438) instead of the original reduced set.
-
-    Loads an ENVI image as a Matlab-like dictionary with spectra (IF) and pixel
-    coordinates (x, y). If the header (.hdr) is not found, it is automatically
-    generated from a .lbl file (using the approach in the
-    `CRISM spectral calculator`_); if neither is available, an error is raised.
-
-    .. _CRISM spectral calculator: https://github.com/jlaura/crism/blob/\
-        master/csas.py
-
-    Parameters
-    ----------
-    fname: str
-        ENVI file to open (.hdr or .img)
-    flatten: bool
-        flatten an image array to (npix, nchan) and saves the coordinates to
-        the x,y fields (default: False)
-
-    Returns
-    -------
-    mat: dict
-        a dictionary storing the spectra and the pixels coordinates (if flatten
-        is True)
+def load_image(filename: str) -> tuple[np.ndarray, envi.SpyFile]:
     """
-    # pylint: disable=import-outside-toplevel
-
-    band_select = np.arange(0, NUM_BANDS, 1)
-    fbase, _ = os.path.splitext(fname)
+    Modified from https://github.com/Banus/crism_ml/blob/master/crism_ml/io.py load_image
+    """
+    fbase, _ = os.path.splitext(filename)
     try:
         img = envi.open(f"{fbase}.hdr")
     except spyfile.FileNotFoundError:
@@ -44,32 +19,4 @@ def crism_to_mat(fname: str, flatten: bool = False) -> dict:
         img = envi.open(f"{fbase}.hdr")
 
     arr = img.load()
-
-    mdict = {"IF": arr[:, :, band_select]}
-    if flatten:  # use coordinate arrays for indexing
-        xx_, yy_ = np.meshgrid(np.arange(arr.shape[1]), np.arange(arr.shape[0]))
-        mdict.update({"x": xx_.ravel() + 1, "y": yy_.ravel() + 1})
-        mdict["IF"] = mdict["IF"].reshape((-1, len(band_select)))
-
-    return mdict
-
-
-def load_image(fname: str) -> dict:
-    """Try to load a .mat file and fall back to ENVI if not found.
-    Modified from https://github.com/Banus/crism_ml/blob/master/crism_ml/io.py
-    Now uses n2n4m.io.crism_to_mat instead of crism_ml.io.crism_to_mat.
-    
-    Parameters
-    ----------
-    fname : str
-        Path to file to load.
-
-    Returns
-    -------
-    image : dict
-        Dictionary of image data.
-    """
-    try:
-        return crism_ml.io.loadmat(fname)
-    except (FileNotFoundError, NotImplementedError, ValueError):
-        return crism_to_mat(fname, flatten=True)
+    return np.array(arr), img
