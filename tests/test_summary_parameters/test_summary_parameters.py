c4c08e2ff2b53e6bf1959ea3b5917e7329cc8a20
Robert Platt <rp1818@ic.ac.uk>
2024-02-07 13:59:36 +0000

    correct function names

--- tests/test_summary_parameters/test_summary_parameters.py
+++ tests/test_summary_parameters/test_summary_parameters.py
@@ -6,10 +6,10 @@ import n2n4m.summary_parameters as summary_parameters
 
 
 def test_wavelength_weights():
-    a, b = summary_parameters.wavelength_weights(1, 2, 3)
+    a, b = summary_parameters._wavelength_weights(1, 2, 3)
     assert a == 0.5
     assert b == 0.5
-    a, b = summary_parameters.wavelength_weights(1, 1, 2)
+    a, b = summary_parameters._wavelength_weights(1, 1, 2)
     assert a == 1
     assert b == 0
 
@@ -19,7 +19,7 @@ def test_interpolated_center_wavelength_reflectance():
     short_ref = np.array([1])
     long_ref = np.array([3])
     interpolated_reflectance = (
-        summary_parameters.interpolated_center_wavelength_reflectance(
+        summary_parameters._interpolated_center_wavelength_reflectance(
             short_ref, wavelengths, long_ref
         )
     )
@@ -32,7 +32,7 @@ def test_band_depth_calculation():
     all_wavelengths = (1, 2, 3)
     bd_wavelengths = (1, 2, 3)
     kernel_sizes = (1, 1, 1)
-    band_depth = summary_parameters.band_depth_calculation(
+    band_depth = summary_parameters._band_depth_calculation(
         spectrum, all_wavelengths, bd_wavelengths, kernel_sizes
     )
     assert type(band_depth) == np.ndarray
