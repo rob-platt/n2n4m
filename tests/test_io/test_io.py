12cf6e7ed0e93e5b4cda232d72e147dddbcfa36c
Robert Platt <robp2602@gmail.com>
2024-02-21 13:18:22 +0000

    Change expected directories

--- tests/test_io/test_io.py
+++ tests/test_io/test_io.py
@@ -6,13 +6,13 @@ import n2n4m.io
 
 @pytest.fixture
 def crism_image():
-    return n2n4m.io.load_image("tests/test_postprocessing/3561F/ATU0003561F_01_IF168L_TRR3.img")
+    return n2n4m.io.load_image("tests/test_io/3561F/ATU0003561F_01_IF168L_TRR3.img")
 
 
 def test_load_images_from_shortcode():
     good_mineral_sample = pd.DataFrame({"Image_Name": "3561F", "Pixel_Class": [10]})
     bad_mineral_sample = pd.DataFrame({"Image_Name": "AAAAA", "Pixel_Class": [10]})
-    good_data_dir = "tests/test_postprocessing"
+    good_data_dir = "tests/test_io"
     bad_data_dir = "tests/"
     image_array = n2n4m.io.load_image_from_shortcode(good_mineral_sample, good_data_dir)
     assert type(image_array) == np.ndarray
--- tests/test_postprocessing/test_postprocessing.py
+++ tests/test_postprocessing/test_postprocessing.py
@@ -1,5 +1,5 @@
 import pytest
-import pandas as pd
+import os
 import numpy as np
 
 import n2n4m.postprocessing as postprocessing
@@ -9,10 +9,12 @@ from n2n4m.preprocessing import ALL_WAVELENGTHS, PLEBANI_WAVELENGTHS
 
 BAND_MASK = [True if band in PLEBANI_WAVELENGTHS else False for band in ALL_WAVELENGTHS]
 
+PACKAGE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+CRISM_ML_DATA_DIR = os.path.join(PACKAGE_DIR, "data", "CRISM_ML")
 
 @pytest.fixture
 def crism_image():
-    return load_image("tests/test_postprocessing/3561F/ATU0003561F_01_IF168L_TRR3.img")
+    return load_image("tests/test_io/3561F/ATU0003561F_01_IF168L_TRR3.img")
 
 
 def test_calculate_pixel_blandness(crism_image):
@@ -21,7 +23,7 @@ def test_calculate_pixel_blandness(crism_image):
     spectra = image_array.reshape(-1, image_array.shape[-1])[:, BAND_MASK] # Flatten image and mask bands not used in Plebani model
 
     blandness_scores = postprocessing.calculate_pixel_blandness(
-        spectra, im_shape=im_shape
+        spectra, im_shape=im_shape, train_set_dir = CRISM_ML_DATA_DIR
     )
     assert type(blandness_scores) == np.ndarray
     assert blandness_scores.shape == im_shape
@@ -29,8 +31,8 @@ def test_calculate_pixel_blandness(crism_image):
         blandness_scores[0, 0] == -np.inf
     )  # Corner pixels are always bad pixels, so should be -np.inf
     pytest.raises(
-        ValueError, postprocessing.calculate_pixel_blandness, spectra[:100], im_shape
+        ValueError, postprocessing.calculate_pixel_blandness, spectra[:100], im_shape, CRISM_ML_DATA_DIR
     )
     pytest.raises(
-        TypeError, postprocessing.calculate_pixel_blandness, im_shape, im_shape
+        TypeError, postprocessing.calculate_pixel_blandness, im_shape, im_shape, CRISM_ML_DATA_DIR
     )
