8cb13e97a0c1af358d67dc91c5f221d544ffc3bd
Robert Platt <rp1818@ic.ac.uk>
2025-03-05 13:53:31 +0000

    fix: included missing sklearn import

--- n2n4m/preprocessing.py
+++ n2n4m/preprocessing.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from sklearn.metrics import r2_score
+# from sklearn.metrics import r2_score
 import numpy as np
 # import torch
 # from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
